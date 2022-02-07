4b7d902c30fb39c051428fd9e0ee3840027b4cd2
星外之神 <wszqkzqk@qq.com>
2022-02-07 17:19:55 +0800

    Updated by update.sh

--- update.sh
+++ update.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
 cd `dirname $0`; pwd
+
+# 指定仓库地址(注意：对于gitee仓库应当用小写字母)
+repoinfo="zqkcho/pubzqkcho.git"
+
+# 添加更改
+echo "正在添加变更文件……"
 git add *
 git add .gitignore
-git pull git@github.com:ZQKCHO/PubZQKCHO.git
 git commit -m 'Updated by update.sh'
-git push -u git@gitee.com:zqkcho/pubzqkcho.git
-git push -u git@github.com:ZQKCHO/PubZQKCHO.git
+
+# 合并远端更改
+echo "正在合并远端更改……"
+git pull git@github.com:${repoinfo}
+
+# 检测是否存在镜像仓库，如果不存在，进行添加
+flag=1
+for i in $(git remote);do
+    if [[ ${i} == mirror ]];then
+        flag=0
+    fi
+done
+
+# 如果不存在镜像仓库，进行添加
+if [[ ${flag} == 1 ]];then
+    git remote add mirror git@gitee.com:${repoinfo}
+fi
+
+# 进行推送
+echo "正在向原仓库推送……"
+git push origin
+echo "正在向镜像仓库推送……"
+git push mirror
